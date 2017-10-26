005d336a2019aeefac27dd49fac79f3d12920ce8
o.lutsevich <oleg.31@mail.ru>
2017-10-26 17:58:57 +0300

    Add Linkedin v3.2

--- src/main/java/com/springsource/petclinic/web/NavigationController.java
+++ src/main/java/com/springsource/petclinic/web/NavigationController.java
@@ -3,10 +3,18 @@ import org.springframework.social.linkedin.api.Companies;
 import org.springframework.social.linkedin.api.Company;
 import org.springframework.social.linkedin.api.LinkedIn;
 import org.springframework.social.linkedin.api.impl.LinkedInTemplate;
+import org.springframework.social.linkedin.connect.LinkedInServiceProvider;
+import org.springframework.social.oauth2.OAuth2Operations;
+import org.springframework.social.oauth2.OAuth2Template;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 
@@ -14,7 +22,7 @@ import java.util.List;
 @Controller
 public class NavigationController {
 
-    public static String token;
+    private static String token;
 
     private static final String ACC_KEY = "771efm4ji87uw6";
     private static final String ACC_SECRET = "UOht0NEI1yvDld8Z";
@@ -33,12 +41,22 @@ public class NavigationController {
 
     @RequestMapping("/linkedin")
     public String showLinkedin(Model model) {
-        LinkedIn linkedin = new LinkedInTemplate(ACC_KEY);
+        if(token == null) {
+            getToken();
+        }
+        LinkedIn linkedin = new LinkedInTemplate(token);
         Companies comp = linkedin.companyOperations().search("jetbi.com");
 
         List<Company> companyList = comp.getCompanies();
         model.addAttribute("companyList", companyList);
         return "linkedin";
+    }
+
+
+    private void getToken() {
+        LinkedInServiceProvider serviceProvider = new LinkedInServiceProvider(ACC_KEY, ACC_SECRET);
+        OAuth2Operations template = serviceProvider.getOAuthOperations();
+        token = template.authenticateClient().getAccessToken();
     }
 
 
