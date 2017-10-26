522b180dfca521ab52f3c70d6935a3156b451186
o.lutsevich <oleg.31@mail.ru>
2017-10-26 17:49:20 +0300

    Add Linkedin v3

--- src/main/java/com/springsource/petclinic/web/NavigationController.java
+++ src/main/java/com/springsource/petclinic/web/NavigationController.java
@@ -17,7 +17,6 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
 
-import static javafx.css.StyleOrigin.USER_AGENT;
 
 
 @Controller
@@ -58,40 +57,6 @@ public class NavigationController {
         this.token = template.authenticateClient().getAccessToken();
     }
 
-
-    // HTTP GET request
-    private void sendGet() throws Exception {
-
-        String url = "http://www.google.com/search?q=mkyong";
-
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-
-        // optional default is GET
-        con.setRequestMethod("GET");
-
-        //add request header
-        String USER_AGENT = "Mozilla/5.0";
-        con.setRequestProperty("User-Agent", USER_AGENT);
-
-        int responseCode = con.getResponseCode();
-        System.out.println("\nSending 'GET' request to URL : " + url);
-        System.out.println("Response Code : " + responseCode);
-
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuilder response = new StringBuilder();
-
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
-
-        //print result
-        System.out.println(response.toString());
-
-    }
 
 
 }
