3b47981a448049dc89d33d0f30654d0159a4702f
o.lutsevich <oleg.31@mail.ru>
2017-10-26 17:39:18 +0300

    Revert "Add Spring Social v2.2.4 temp"
    This reverts commit 18c9115de271c91bc750d73d9c98066c12f497d1.

--- src/main/java/com/springsource/petclinic/web/NavigationController.java
+++ src/main/java/com/springsource/petclinic/web/NavigationController.java
@@ -15,6 +15,7 @@ import java.util.List;
 @Controller
 public class NavigationController {
 
+    private static String token;
 
     private static final String ACC_KEY = "771efm4ji87uw6";
     private static final String ACC_SECRET = "UOht0NEI1yvDld8Z";
@@ -33,9 +34,20 @@ public class NavigationController {
 
     @RequestMapping("/linkedin")
     public String showLinkedin(Model model) {
+        LinkedIn linkedin = new LinkedInTemplate(token);
+        Companies comp = linkedin.companyOperations().search("jetbi.com");
+        if(token == null)
+            getToken();
+        List<Company> companyList = comp.getCompanies();
+        model.addAttribute("companyList", companyList);
         return "linkedin";
     }
 
+    private void getToken() {
+        LinkedInServiceProvider serviceProvider = new LinkedInServiceProvider(ACC_KEY, ACC_SECRET);
+        OAuth2Operations template = serviceProvider.getOAuthOperations();
+        this.token = template.authenticateClient().getAccessToken();
+    }
 
 
 }
